09afb5d4f834a0e05a8fabbd9e3b788128449299
Yassine Cherni <125700497+yassine-cherni@users.noreply.github.com>
2024-02-03 04:01:40 +0100

    Update Lab1.py

--- Week1/Lab1.py
+++ Week1/Lab1.py
@@ -1,9 +1,9 @@
-import numpy as np   # used for numerical operations and array manipulations
-import matplotlib.pyplot as plt
-from tqdm import tqdm
-import time
+import numpy as np   #used for numerical operations and array manipulations
+import matplotlib.pyplot as plt #library for creating visualizations
+from tqdm import tqdm #which provides a progress bar to track the progress of loops or tasks
+import time #access functionalities related to time
 
-from rlglue.rl_glue import RLGlue
+from rlglue.rl_glue import RLGlue #RL-Glue (Reinforcement Learning Glue) provides a standard interface that allows you to connect reinforcement learning agents, environments, and experiment programs together, even if they are written in different languages
 import main_agent
 import ten_arm_env
 import test_env
