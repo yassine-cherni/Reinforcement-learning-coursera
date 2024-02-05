26157a79f0c4d884ff2d7734f1823b47fd0f9042
Yassine Cherni <125700497+yassine-cherni@users.noreply.github.com>
2024-02-05 06:51:06 +0100

    Create Define.py

---
+++ MDP-Robotics/Define.py
@@ -0,0 +1,13 @@
+# mdp_components.py Define state, Action, Reward 
+class State:   
+    def __init__(self, name):
+        self.name = name
+
+class Action:
+    def __init__(self, name):
+        self.name = name
+
+class Reward:
+    def __init__(self, value):
+        self.value = value
+      
