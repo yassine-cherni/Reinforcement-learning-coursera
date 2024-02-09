e41b14699aa6cf598074bfaaa1ba57d9d47177a7
Yassine Cherni <125700497+yassine-cherni@users.noreply.github.com>
2024-02-09 21:07:28 +0100

    Update Define.py

--- MDP-Robotics/Define.py
+++ MDP-Robotics/Define.py
@@ -11,4 +11,4 @@ class Reward:
     def __init__(self, value):
         self.value = value
       
-# and the algorithme will learn step by step 
+# and the algorithme will learn step by step like Q-learning 
