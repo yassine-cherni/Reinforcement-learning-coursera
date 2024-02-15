32e9b7db187e093dee1381459b114815b1048fd4
Yassine Cherni <125700497+yassine-cherni@users.noreply.github.com>
2024-02-15 21:12:26 +0100

    Update Lab1-Library.py

--- Week1/Lab1-Library.py
+++ Week1/Lab1-Library.py
@@ -4,6 +4,6 @@ from tqdm import tqdm #which provides a progress bar to track the progress of lo
 import time #access functionalities related to time
 
 from rlglue.rl_glue import RLGlue #RL-Glue (Reinforcement Learning Glue) provides a standard interface that allows you to connect reinforcement learning agents, environments, and experiment programs together, even if they are written in different languages
-import main_agen
+import main_agent
 import ten_arm_env
 import test_env
